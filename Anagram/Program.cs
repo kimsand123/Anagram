b66a21c30c3246c0305aef21d6b5d36144f1b1ae
Kim Sandberg <s@ndberg.dk>
2022-01-17 18:19:45 +0100

    Done. Needs Unittesting

--- Anagram/Program.cs
+++ Anagram/Program.cs
@@ -4,9 +4,29 @@ namespace Anagram
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StringLogic _stringHandler = new();
+
+            Console.Write("Indtast streng 1: ");
+            string string1 = Console.ReadLine();
+            Console.Write("Indtast streng 2: ");
+            string string2 = Console.ReadLine();
+
+            string cleanedString1 = _stringHandler.cleanString(string1);
+            string cleanedString2 = _stringHandler.cleanString(string2);
+
+            if (_stringHandler.compareStrings(cleanedString1, cleanedString2))
+            {
+                Console.WriteLine("\n De to strenge er anagrammer.");
+            }
+            else
+            {
+                Console.WriteLine("\nDe to strenge er ikke anagrammer.");
+            }
+
+
         }
     }
 }
---
+++ Anagram/StringLogic.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IAsyncResult = System.IAsyncResult;
+
+namespace Anagram
+{
+    internal class StringLogic
+    {
+        internal StringLogic()
+        {
+
+        }
+
+        internal string cleanString(string stringToBeCleaned)
+        {
+            return stringToBeCleaned.Replace(" ", "").toup;
+
+        }
+
+        internal bool compareStrings(string string1, string string2)
+        {
+            // Når mellemrummene er fjernet, skal strengene være lige lange
+            // Da de skal bestå af præcis de samme bogstaver for at være anagrammer.
+            int balance = 0;
+            bool result = false;
+
+            if (string1.Length == string2.Length)
+            {
+
+                
+                for (int counter = 0; counter < string1.Length; counter++)
+                {
+                    balance = balance + (int) string1[counter] - (int) string2[counter];
+                }
+
+                if (balance == 0)
+                {
+                    result = true;
+                }
+
+            }
+
+            return result;
+        }
+    }
+}
